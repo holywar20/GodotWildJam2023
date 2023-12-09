a44e3cfca635567ec541a8a1a0c3454d0c3f82eb
Duncan Brown <duncan@prometheussoftware.ca>
2023-12-09 11:37:56 -0500

    wip: removes duplicate event bus; adds initial signals for event bus; updates/adds resources and building types; mild reorg

--- autoloads/event_bus.gd
+++ autoloads/event_bus.gd
@@ -1,3 +1,25 @@
 extends Node
 
 
+# Building signals
+signal construction_started(building)
+signal constructed(building)
+
+
+# Planet signals
+signal planet_empty(planet)
+
+
+# Event signals
+signal event_happened(event_type)
+signal event_started(event_type)
+signal event_ended(event_type)
+
+
+# Star signals
+signal star_transitioned
+
+
+# Misc. game signals
+signal game_paused
+signal game_unpaused
---
+++ entities/buildings/starlifter/starlifter.gd
@@ -0,0 +1,2 @@
+class_name Starlifter
+extends AbstractBuilding
