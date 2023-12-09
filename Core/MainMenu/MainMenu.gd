713c41862a4acd610b008b07708170d06f40d33a
WolfKolona <97687102+WolfKolona@users.noreply.github.com>
2023-12-09 06:09:15 +0000

    Initial scenes with main menu, star shaders and starscape
    I require recharging of my meat machine

---
+++ Core/MainMenu/MainMenu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _ready():
+	pass # Replace with function body.
+
+
+func _on_newGame_pressed():
+	pass # Replace with function body.
+
+
+func _on_quit_pressed():
+	pass # Replace with function body.
