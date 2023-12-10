885433f588741205e758657d841714ccf7145954
Duncan Brown <duncan@prometheussoftware.ca>
2023-12-10 12:41:47 -0500

    wip: adds temporary planet cracker and temporary scaffolding for testing resource reporting

--- Core/constants/constants.gd
+++ Core/constants/constants.gd
@@ -4,7 +4,7 @@ class_name Constants
 
 # Building tiers
 enum Tiers {
-	TEIR_0,
+	TIER_0,
 	TIER_1,
 	TIER_2,
 	TIER_3
@@ -22,3 +22,11 @@ const BUILDING_ANTIMATTER_FACTORY = "Antimatter Factory"
 const BUILDING_STELLAR_ACCELERATOR = "Stellar Accelerator"
 const BUILDING_STARLIFTER = "Starlifter"
 
+
+# Resource types
+const ANTIMATTER = preload("res://entities/resources/antimatter.tres")
+const BASE_METAL = preload("res://entities/resources/base_metal.tres")
+const HYDROGEN = preload("res://entities/resources/hydrogen.tres")
+const POWER = preload("res://entities/resources/power.tres")
+const PRECIOUS_METAL = preload("res://entities/resources/precious_metal.tres")
+const UNOBTANIUM = preload("res://entities/resources/unobtanium.tres")
--- entities/buildings/planet_cracker/planet_cracker.gd
+++ entities/buildings/planet_cracker/planet_cracker.gd
@@ -1,2 +1,14 @@
 class_name PlanetCracker
 extends AbstractBuilding
+
+
+@onready var planet = get_node("/root/ROOT/SolarSystem/Planets/P1")
+
+
+func _ready():
+	super()
+	_is_constructed = true
+	is_active = true
+	produces[Constants.HYDROGEN] = 10
+
+
