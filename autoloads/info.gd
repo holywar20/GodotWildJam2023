2bf9ebf3afd1de9ff223c8049b001d27f8f94545
Duncan Brown <duncan@prometheussoftware.ca>
2023-12-13 15:47:43 -0500

    fix: instantiates scenes instead of scripts

--- autoloads/info.gd
+++ autoloads/info.gd
@@ -3,13 +3,14 @@ extends Node
 
 # Building info
 var BUILDING_INFO = [
-	AntimatterFactory.new(),
-	CelestialExtractor.new(),
-	DysonSwarm.new(),
-	FusionReactor.new(),
-	Gigafactory.new(),
-	MagneticBore.new(),
-	PlanetCracker.new(),
-	Starlifter.new(),
-	StellarAccelerator.new()
+	preload("res://entities/buildings/antimatter_factory/antimatter_factory.tscn").instantiate(),
+	preload("res://entities/buildings/antimatter_factory/antimatter_factory.tscn").instantiate(),
+	preload("res://entities/buildings/celestial_extractor/celestial_extractor.tscn").instantiate(),
+	preload("res://entities/buildings/dyson_swarm/dyson_swarm.tscn").instantiate(),
+	preload("res://entities/buildings/fusion_reactor/fusion_reactor.tscn").instantiate(),
+	preload("res://entities/buildings/gigafactory/gigafactory.tscn").instantiate(),
+	preload("res://entities/buildings/magnetic_bore/magnetic_bore.tscn").instantiate(),
+	preload("res://entities/buildings/planet_cracker/planet_cracker.tscn").instantiate(),
+	preload("res://entities/buildings/starlifter/starlifter.tscn").instantiate(),
+	preload("res://entities/buildings/stellar_accelerator/stellar_accelerator.tscn").instantiate(),
 ]
