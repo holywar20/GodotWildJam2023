1a9d16cbc8e282dbac6f2fded0e0b099e4eafdc1
WolfKolona <97687102+WolfKolona@users.noreply.github.com>
2023-12-13 20:37:44 +0000

    Send Assistance

--- SharedUI/UIButtonsAndPanels/BuildingButton.gd
+++ SharedUI/UIButtonsAndPanels/BuildingButton.gd
@@ -4,13 +4,17 @@ extends Button
 
 @onready var buildingName = $HBox/BuildingName
 
-var building
 var buildingArray = Info.BUILDING_INFO
+var building 
 
 func _ready():
+	building = buildingArray.filter(func (b): return b.type == Constants.BUILDING_GIGAFACTORY)[0]
 	match buildingRef:
 		"Gigafactory":
 			buildingName.set_text(buildingRef)
+			#for info in buildingArray:
+				#if info.type == Constants.BUILDING_GIGAFACTORY:
+					#building = info
 		"Fusion Reactor":
 			buildingName.set_text(buildingRef)
 		"Celestial Extractor":
@@ -28,4 +32,5 @@ func _ready():
 
 
 func _on_pressed():
+	print(building)
 	EventBus.emit_signal("construction_requested", buildingRef)
