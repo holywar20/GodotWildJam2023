df70257d108bb701de0ccae4a19627da5a178d14
Duncan Brown <duncan@prometheussoftware.ca>
2023-12-14 00:27:07 -0500

    wip: cleanup

--- SharedUI/feedback_message_scene.gd
+++ SharedUI/feedback_message_scene.gd
@@ -1,15 +1,7 @@
 extends HBoxContainer
 
 @onready var messageBox = $Control
-@onready var animPlayer = $AnimationPlayer
 
-func _ready():
-	pass
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 
 func beginMessage(nText, pos):
 	messageBox.text = nText
