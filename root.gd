6eba9b3a8e82bca7f08836542aa1a39f9560a323
Duncan Brown <duncan@prometheussoftware.ca>
2023-12-13 22:14:18 -0500

    wip: disables intro flow

--- root.gd
+++ root.gd
@@ -2,11 +2,11 @@ extends Node2D
 
 
 func _ready() -> void:
-	get_tree().paused = true
+	#get_tree().paused = true
 	EventBus.connect("planet_nav_button_pressed", Callable(self, "_on_EB_planet_nav_button_pressed"))
 	EventBus.connect("return_to_star_pressed", Callable(self, "_on_EB_return_to_star_pressed"))
 	EventBus.new_game.connect(_on_new_game)
-	AudioManager.play_music(AudioManager.MUSIC_TRACK_TITLE)
+	#AudioManager.play_music(AudioManager.MUSIC_TRACK_TITLE)
 
 func _on_EB_planet_nav_button_pressed(planetRef):
 	var moveTween = create_tween()
@@ -26,5 +26,5 @@ func _on_EB_return_to_star_pressed():
 
 
 func _on_new_game() -> void:
-	AudioManager.play_music(AudioManager.MUSIC_TRACK_GAME_1)
+	#AudioManager.play_music(AudioManager.MUSIC_TRACK_GAME_1)
 	EventBus.game_unpaused.emit()
