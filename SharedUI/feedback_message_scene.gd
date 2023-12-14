02d775dc3a5faeb423eef1b046b7d2198ba935ac
Duncan Brown <duncan@prometheussoftware.ca>
2023-12-14 15:04:54 -0500

    wip: adds fade to feedback message

--- SharedUI/feedback_message_scene.gd
+++ SharedUI/feedback_message_scene.gd
@@ -5,8 +5,12 @@ extends HBoxContainer
 
 func beginMessage(nText, pos):
 	messageBox.text = nText
-	#print(pos)
 	set_position(pos)
+
+	# Have the text "rise"
 	var tween = create_tween()
-	tween.tween_property(self, "position", position - Vector2(0, 100), 0.75)
+	tween.tween_property(self, "position", position - Vector2(0, 100), 1.0)
 	tween.tween_callback(queue_free)
+
+	# Have the text fade out
+	create_tween().tween_property(self, "modulate", Color.TRANSPARENT, 1.0)
