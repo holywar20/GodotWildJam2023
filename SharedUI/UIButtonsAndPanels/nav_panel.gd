f960326a0961290f59ae63da419c25169dfd6397
Duncan Brown <duncan@prometheussoftware.ca>
2023-12-15 21:32:04 -0500

    feat: tiers the planets via nav buttons; makes the planet nav panel semi-transparent; updates some wording for the star transitions

--- SharedUI/UIButtonsAndPanels/nav_panel.gd
+++ SharedUI/UIButtonsAndPanels/nav_panel.gd
@@ -1,10 +1,22 @@
 extends PanelContainer
 
+
+const PLANET_TIER_GROUPS = {
+	1: "TIER_1_PLANETS",
+	2: "TIER_2_PLANETS",
+	3: "TIER_3_PLANETS"
+}
+
+
 @onready var navButtonContainer = $VBox/NavContainer
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
-	pass # Replace with function body.
+	EventBus.star_transitioned.connect(_on_star_transitioned)
+
+	for planet_button in get_tree().get_nodes_in_group("TIER_1_PLANETS"):
+		planet_button.visible = true
+
 
 func updateUI():
 	var planetArray = get_tree().get_nodes_in_group( "PLANET_SCENE" )
@@ -19,3 +31,11 @@ func updateUI():
 
 func _on_star_pressed():
 	EventBus.emit_signal("return_to_star_pressed")
+
+
+func _on_star_transitioned(tier) -> void:
+	if not PLANET_TIER_GROUPS.has(tier + 1):
+		return
+
+	for planet_button in get_tree().get_nodes_in_group(PLANET_TIER_GROUPS[tier + 1]):
+		planet_button.visible = true
