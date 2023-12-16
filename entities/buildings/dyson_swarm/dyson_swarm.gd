1d25d46a16c6e6b72adab9c8dc490e02c3f49c2e
Duncan Brown <duncan@prometheussoftware.ca>
2023-12-16 11:49:22 -0500

    chore: cleanup testing/dead code

--- entities/buildings/dyson_swarm/dyson_swarm.gd
+++ entities/buildings/dyson_swarm/dyson_swarm.gd
@@ -4,7 +4,3 @@ extends AbstractBuilding
 # Overrides!
 func _set_nodes() -> void:
 	pass # Dyson swarm does not need sprites. TODO : Set up particle effects here.
-
-
-func _on_button_pressed():
-	EventBus.emit_signal("building_pressed", self)
