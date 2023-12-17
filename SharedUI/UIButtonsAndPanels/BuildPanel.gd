7a93db6fb162f2c503812e6754fa066a475cae81
Duncan Brown <duncan@prometheussoftware.ca>
2023-12-17 00:53:43 -0500

    feat: adds tooltips to structure construction menu

--- SharedUI/UIButtonsAndPanels/BuildPanel.gd
+++ SharedUI/UIButtonsAndPanels/BuildPanel.gd
@@ -10,7 +10,8 @@ extends PanelContainer
 @onready var AntimatterFactoryButton = $VBox/ScrollContainer/VBoxContainer/AntimatterFactory
 @onready var StellarAcceleratorButton = $VBox/ScrollContainer/VBoxContainer/StellarAccelerator
 @onready var StarlifterButton = $VBox/ScrollContainer/VBoxContainer/Starlifter
-
+@onready var GigafactoryButton = $VBox/ScrollContainer/VBoxContainer/Gigafactory
+@onready var FusionReactorButton = $VBox/ScrollContainer/VBoxContainer/FusionReactor
 
 var isOpen = false
 
@@ -22,6 +23,14 @@ var currentTier = 1
 func _ready():
 	# Joe this code breaks on star_transitioned
 	EventBus.connect("star_transitioned", Callable(self, "_on_EB_star_transitioned"))
+	GigafactoryButton.tooltip_text = Constants.BUILDING_GIGAFACTORY_DESCRIPTION
+	FusionReactorButton.tooltip_text = Constants.BUILDING_FUSION_REACTOR_DESCRIPTION
+	CelestialExtractorButton.tooltip_text = Constants.BUILDING_CELESTIAL_EXTRACTOR_DESCRIPTION
+	DysonSwarmButton.tooltip_text = Constants.BUILDING_DYSON_SWARM_DESCRIPTION
+	MagneticBoreButton.tooltip_text = Constants.BUILDING_MAGNETIC_BORE_DESCRIPTION
+	StellarAcceleratorButton.tooltip_text = Constants.BUILDING_STELLAR_ACCELERATOR_DESCRIPTION
+	StarlifterButton.tooltip_text = Constants.BUILDING_STARLIFTER_DESCRIPTION
+
 
 func _on_EB_star_transitioned(_state):
 	currentTier = _state + 1
