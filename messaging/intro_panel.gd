39490726eef258683b675b53072b375767af7b4b
Bryan <bryan.winter@idt.net>
2023-12-17 14:01:37 -0500

    Implimented Intro screen

--- messaging/intro_panel.gd
+++ messaging/intro_panel.gd
@@ -2,11 +2,11 @@ extends Panel
 
 
 @onready var start_button = $Control3/StartButton
-@onready var description = $Control2/Description
 
 
 func _ready() -> void:
-	description.text = StellarConstants.get_tier_metadata(0).description
+	# description.text = StellarConstants.get_tier_metadata(0).description
+	pass
 
 func _on_start_button_pressed():
 	EventBus.new_game.emit()
